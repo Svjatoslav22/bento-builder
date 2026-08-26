e2c9221ae8a01661a05fca183d8113c98065d89a
Sviatoslav Tymtsiv <svtm1234567@gmail.com>
2026-08-26 16:44:51 +0300

    fix: update port config for render

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  // Дозволяємо Render задавати свій порт
+  const port = process.env.PORT || 3000;
+  // Слухаємо всі інтерфейси (0.0.0.0), це обов'язкова умова для хмарних хостингів
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
