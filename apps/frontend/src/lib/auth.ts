524d26df6915cd6b3c0e39701efb017b91b30363
Sviatoslav Tymtsiv <svtm1234567@gmail.com>
2026-09-02 14:46:46 +0300

    add new field to prisma schema and apply migration

--- apps/frontend/src/lib/auth.ts
+++ apps/frontend/src/lib/auth.ts
@@ -11,10 +11,12 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      allowDangerousEmailAccountLinking: true,
     }),
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+      allowDangerousEmailAccountLinking: true,
     }),
   ],
   session: {
